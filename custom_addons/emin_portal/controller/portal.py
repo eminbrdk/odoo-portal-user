5f9183baedee70cf69cdd554bc071e782234ad31
eminbrdk <eminbrdk4@gmail.com>
2024-02-09 20:47:47 +0300

    9 şubat

--- custom_addons/emin_portal/controller/portal.py
+++ custom_addons/emin_portal/controller/portal.py
@@ -1,4 +1,4 @@
-from odoo.addons.portal.controllers.portal import CustomerPortal
+from odoo.addons.portal.controllers.portal import CustomerPortal, pager
 from odoo.http import request
 from odoo import http
 
@@ -6,10 +6,10 @@ class WeblearnsPortal(CustomerPortal):
 
     def _prepare_home_portal_values(self, counters):
         rtn = super(WeblearnsPortal, self)._prepare_home_portal_values(counters)
-        rtn["student_counts"] = request.env["school.student"].search_count([])
+        rtn["student_counts"] = request.env["school.student"].sudo().search_count([])
         return rtn
 
-    @http.route(["/my/students", "/my/students/page/<int:page>"], type="http", website=True)
+    @http.route(["/my/students", "/my/students/page/<int:page>"], auth="user", type="http", website=True)
     def wb_student_list_view_portal(self, page=1, sortby="id", **kw):
         sorted_list = {
             "id": {"label": "ID Decs", "order": "id desc"},
@@ -17,21 +17,11 @@ class WeblearnsPortal(CustomerPortal):
             "school_id": {"label": "School", "order": "school_id"},
         }
         default_order_by = sorted_list[sortby]["order"]
-        students = request.env["school.student"].search([], limit=5, order=default_order_by)
+        students = request.env["school.student"].sudo().search([], limit=5, order=default_order_by)
         vals = {"students": students, "page_name": "students_list_view", "sortby": sortby, "searchbar_sortings": sorted_list}
         return request.render("emin_portal.wb_student_list_view_portal", vals)
 
-    def small_button_pressed(self):
-        print("smalll")
-        return "yusuf-5"
-
-    def big_button_pressed(self):
-        print("bigggg")
-        return "yusuf-5"
-
-    @http.route(['/my/students/<model("school.student"):student_id>'], type="http", website=True)
+    @http.route(['/my/students/<model("school.student"):student_id>'], type="http", website=True, auth="user")
     def web_learns_student_form_view_portal(self, student_id, **kw):
-        big = self.big_button_pressed()
-        small = self.small_button_pressed()
-        vals = {"student": student_id, "page_name": "student_form_view", "big_button": big, "small_button": small}
+        vals = {"student": student_id, "page_name": "student_form_view"}
         return request.render("emin_portal.web_learns_student_form_view_portal", vals)
